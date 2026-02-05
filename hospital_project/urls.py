d1083a4013f7a2bad149f703b35c1eb5f3a90108
Shadil Ali <shadilali@Shadils-MacBook-Pro.local>
2026-02-05 19:16:48 +0530

    Fix unmatched bracket in urls.py

--- hospital_project/urls.py
+++ hospital_project/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
 # Serve static files in production (temporary workaround)
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-]
 
